549a9717705d0ebfdade84fa596973a4612bcc69
Nana <2190138@talnet.nl>
2024-12-05 12:30:42 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('/planeten', function () {
+    return response()->json([
+        "Uranus",
+        "Jupiter",
+        "Mars",
+        "Aarde",
+        "Saturnus",
+        "Pluto",
+        "Neptunus",
+        "Venus"
+    ]);
+});
