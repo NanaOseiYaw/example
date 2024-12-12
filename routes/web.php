94cb50b4b61ad08d52e2817d5d1dc54579f47085
Nana <2190138@talnet.nl>
2024-12-12 12:19:45 +0100

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/planeten', function () {
-    return response()->json([
+    $planeten = collect([
         "Uranus",
         "Jupiter",
         "Mars",
@@ -12,4 +13,19 @@ Route::get('/planeten', function () {
         "Neptunus",
         "Venus"
     ]);
+
+    // Controleer of er een 'planet' parameter is en filter de collectie indien nodig
+    if (request()->has('planet')) {
+        $planetNaam = strtolower(request('planet'));
+
+        $filterplaneten = $planeten->filter(function ($planet) use ($planetNaam) {
+            return strtolower($planet) === $planetNaam;
+        });
+
+        // Retourneer het gefilterde resultaat als JSON
+        return response()->json($filterplaneten->values());
+    }
+
+    // Retourneer alle planeten als er geen filter is
+    return response()->json($planeten);
 });
