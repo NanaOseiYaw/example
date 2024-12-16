8ed2c4b867b5a5c9ec022902b034fb135abfa2ec
Nana <2190138@talnet.nl>
2024-12-16 13:32:27 +0100

    Done

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ Route::get('/planeten', function () {
         "Venus"
     ]);
 
-    // Controleer of er een 'planet' parameter is en filter de collectie indien nodig
     if (request()->has('planet')) {
         $planetNaam = strtolower(request('planet'));
 
@@ -22,10 +21,31 @@ Route::get('/planeten', function () {
             return strtolower($planet) === $planetNaam;
         });
 
-        // Retourneer het gefilterde resultaat als JSON
         return response()->json($filterplaneten->values());
     }
 
-    // Retourneer alle planeten als er geen filter is
     return response()->json($planeten);
+});
+
+Route::get('/planets', function () {
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    return view('planets.index', ['planets' => $planets]);
 });
